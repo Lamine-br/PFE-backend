521da6af1f4e598534fe9d4cd9f06554ed97fa1c
Lamine-br <jl_brahami@esi.dz>
2024-04-02 23:48:50 +0100

    abonnements function added

--- service_abonnements/service.js
+++ service_abonnements/service.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const Employeur = require("../models/employeur");
+const Abonnement = require("../models/abonnement");
 const Contact = require("../models/contact");
 const Chercheur = require("../models/chercheur");
 const connectDB = require("../database/connectDB");
@@ -16,163 +16,93 @@ service.use(cors({ origin: "*" }));
 service.use(express.urlencoded({ extended: true }));
 service.use(express.json());
 
-service.get("/profile", verifyAccessToken, async (req, res) => {
-	const type = req.decoded.payloadAvecRole.type;
-	const userId = req.decoded.payloadAvecRole._id;
-	switch (type) {
-		case "employeur":
-			try {
-				const employeur = await Employeur.findById(userId).populate("contacts");
-				if (!employeur) {
-					return res.status(404).json("Employeur non trouvé");
-				}
-				res.status(200).json(employeur);
-				break;
-			} catch (error) {
-				return res.status(500).json({ message: "Internal server error" });
-			}
-
-		case "chercheur":
-			try {
-				const chercheur = await Chercheur.findById(userId);
-				if (!chercheur) {
-					return res.status(404).json("Chercheur non trouvé");
-				}
-				res.status(200).json(chercheur);
-				break;
-			} catch (error) {
-				return res.status(500).json({ message: "Internal server error" });
-			}
+service.get("/abonnements", async (req, res) => {
+	try {
+		const abonnements = await Abonnement.find();
+		return res.status(200).json(abonnements);
+	} catch (error) {
+		console.log(error);
+		return res.status(500).json({ message: "Internal server error" });
 	}
 });
 
-service.put("/profile", verifyAccessToken, async (req, res) => {
-	const userId = req.decoded.payloadAvecRole._id;
-	const type = req.decoded.payloadAvecRole.type;
+service.get("/abonnements/:id", async (req, res) => {
+	const metierId = req.params.id;
+	try {
+		const abonnement = await Abonnement.findOne({ _id: metierId });
 
-	switch (type) {
-		case "chercheur":
-			try {
-				const {
-					email,
-					newPassword,
-					oldPassword,
-					nom,
-					prenom,
-					nationalite,
-					ville,
-					numero,
-				} = req.body;
-				const chercheur = await Chercheur.findById(userId);
-				if (!chercheur) {
-					return res.status(404).json({ message: "Chercheur introuvable" });
-				}
-				if (email) chercheur.email = email;
+		if (!abonnement) {
+			return res.status(404).json({ message: "Abonnement non trouvé" });
+		}
+		return res.status(200).json(abonnement);
+	} catch (error) {
+		console.log(error);
+		return res.status(500).json({ message: "Internal server error" });
+	}
+});
 
-				if (newPassword) {
-					console.log(chercheur, newPassword);
-					const isPasswordValid = await bcrypt.compare(
-						oldPassword,
-						chercheur.password
-					);
-					if (isPasswordValid) {
-						chercheur.password = await bcrypt.hash(newPassword, 10);
-					} else {
-						return res.status(400).json({ message: "Mot de passe incorrect" });
-					}
-				}
+service.post("/abonnements/add", async (req, res) => {
+	const { nom, duree, prix, conditions, avantages } = req.body;
 
-				if (nom) chercheur.nom = nom;
-				if (prenom) chercheur.prenom = prenom;
-				if (nationalite) chercheur.nationalite = nationalite;
-				if (ville) chercheur.ville = ville;
-				if (numero) chercheur.numero = numero;
+	try {
+		const abonnement = new Abonnement({
+			nom,
+			duree,
+			prix,
+			conditions,
+			avantages,
+		});
 
-				await chercheur.save();
-				return res.status(200).json({
-					user: {
-						type: "chercheur",
-						email: chercheur.email,
-						username: chercheur.nom + " " + chercheur.prenom,
-						image: "",
-					},
-				});
-			} catch (error) {
-				console.error(error);
-				return res.status(500).json({ message: "Internal server error" });
-			}
-		case "employeur":
-			try {
-				const {
-					email,
-					oldPassword,
-					newPassword,
-					entreprise,
-					service,
-					sous_service,
-					numero_EDA,
-					site_web,
-					linkedin,
-					facebook,
-					rue,
-					ville,
-					contact,
-				} = req.body;
-				const employeur = await Employeur.findById(userId);
-				if (!employeur) {
-					return res.status(404).json({ message: "Employeur introuvable" });
-				}
-				if (email) employeur.email = email;
-				if (newPassword) {
-					console.log(employeur, newPassword);
-					const isPasswordValid = await bcrypt.compare(
-						oldPassword,
-						employeur.password
-					);
-					if (isPasswordValid) {
-						employeur.password = await bcrypt.hash(newPassword, 10);
-					} else {
-						return res.status(400).json({ message: "Mot de passe incorrect" });
-					}
-				}
+		const abonnementErgst = await abonnement.save();
 
-				if (entreprise) employeur.entreprise = entreprise;
-				if (service) employeur.service = service;
-				if (sous_service) employeur.sous_service = sous_service;
-				if (numero_EDA) employeur.numero_EDA = numero_EDA;
-				if (site_web) employeur.site_web = site_web;
-				if (facebook) employeur.facebook = facebook;
-				if (linkedin) employeur.linkedin = linkedin;
-				if (rue) employeur.adresse.rue = rue;
-				if (ville) employeur.adresse.ville = ville;
+		console.log("Abonnement ajouté");
+		return res.status(201).json(abonnementErgst);
+	} catch (error) {
+		return res.status(500).json({ message: "Internal server error" });
+	}
+});
 
-				if (contact) {
-					const existingContact = await Contact.findById(contact.id);
-					if (contact.nom) {
-						existingContact.nom = contact.nom;
-					}
-					if (contact.email) {
-						existingContact.email = contact.email;
-					}
-					if (contact.numero) {
-						existingContact.numero = contact.numero;
-					}
-					await existingContact.save();
-				}
+service.put("/abonnements/:id", async (req, res) => {
+	const { nom, duree, prix, conditions, avantages } = req.body;
+	const id_abonnement = req.params.id;
 
-				await employeur.save();
-				return res.status(200).json({
-					user: {
-						type: "employeur",
-						email: employeur.email,
-						username: employeur.entreprise,
-						image: "",
-					},
-				});
-			} catch (error) {
-				console.error(error);
-				return res.status(500).json({ message: "Internal server error" });
-			}
+	try {
+		const abonnement = await Abonnement.findById(id_abonnement);
+
+		if (!abonnement) {
+			return res.status(404).json({ message: "Abonnement non trouvé" });
+		}
+
+		abonnement.nom = nom || abonnement.nom;
+		abonnement.duree = duree || abonnement.duree;
+		abonnement.prix = prix || abonnement.prix;
+		abonnement.conditions = conditions || abonnement.conditions;
+		abonnement.avantages = avantages || abonnement.avantages;
+
+		const nouvelAbonnement = await abonnement.save();
+
+		console.log("Abonnement mise à jour");
+		return res.status(200).json(nouvelAbonnement);
+	} catch (error) {
+		console.error("Erreur lors de la modification :", error);
+		return res.status(500).json({ message: "Internal server error" });
+	}
+});
+
+service.delete("/abonnements/:id", async (req, res) => {
+	const id_abonnement = req.params.id;
+
+	try {
+		const result = await Abonnement.deleteOne({ _id: id_abonnement });
+
+		if (result.deletedCount === 0) {
+			return res.status(404).json({ message: "Abonnement non trouvé" });
+		}
+		console.log("Abonnement supprimé");
+		return res.status(200).json({ message: "Abonnement supprimé avec succès" });
+	} catch (error) {
+		console.error(error);
+		return res.status(500).json({ message: "Internal server error" });
 	}
 });
 
