fc437846c00d4e4ccf22c59d0ecac9f7bfaabb36
Lamine-br <jl_brahami@esi.dz>
2024-04-08 00:34:36 +0100

    added function to registrate in service registry

--- service_authentication/service.js
+++ service_authentication/service.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
-const connectDB = require("../database/connectDB");
+const connectDB = require("./database/connectDB");
 const authRouter = require("./routers/authRouter");
+const axios = require("axios");
 
 const service = express();
-const PORT = 3000;
+const PORT = 3001;
 
 service.use(cors({ origin: "*" }));
 service.use(express.urlencoded({ extended: true, limit: "50mb" }));
@@ -16,5 +17,23 @@ service.use(express.static("public"));
 service.use("/auth", authRouter);
 
 connectDB();
+
+service.use((req, res, next) => {
+	console.log(`Request received: ${req.method} ${req.url}`);
+	next();
+});
+
+const registerService = async (serviceName, serviceVersion, servicePort) => {
+	try {
+		const response = await axios.put(
+			`http://localhost:3003/register/${serviceName}/${serviceVersion}/${servicePort}`
+		);
+		console.log(response.data); // Log the response from the registry service
+	} catch (error) {
+		console.error("Error registering service:", error);
+	}
+};
+
+registerService("auth", "v1", PORT);
 
 service.listen(PORT, () => console.log("Service is running at port " + PORT));
